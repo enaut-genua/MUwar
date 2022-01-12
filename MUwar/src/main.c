d063f722b31f44f57426605416bbc709fafb750a
Aritz <Ari_urd_lar@outlook.com>
2022-01-12 12:02:52 +0100

    Init funtzioa sortu da

--- MUwar/src/main.c
+++ MUwar/src/main.c
@@ -7,21 +7,36 @@
 
 #include "tropa.h"
 #include "baldosa.h"
+bool init(SDL_Window* window, SDL_Renderer* renderer, char* titulo, int xpos, int ypos, int width, int height, bool fullscreen);
 
 int main(void)
 {
 	SDL_Window* window = NULL;
 	SDL_Renderer* renderer = NULL;
-	bool isRunning;
+	bool jokoaHasi=false;
 
-	SDL_Init(SDL_INIT_EVERYTHING);
+	jokoaHasi = init( window, renderer,"MUwar", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 700, false);
 
-	window = SDL_CreateWindow("MUwar", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+	while (true)
+	{
 
-	SDL_Delay(8000);
+	}
 
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
 	return 0;
+}
+bool init(SDL_Window* window, SDL_Renderer* renderer,char* titulo, int xpos, int ypos, int width, int height, bool fullscreen) {
+	int flags = 0;
+	bool ret = false;
+	if (fullscreen)flags = SDL_WINDOW_FULLSCREEN_DESKTOP;
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+		window = SDL_CreateWindow(titulo, xpos, ypos, width, height, flags | SDL_WINDOW_RESIZABLE | SDL_WINDOW_FOREIGN);
+		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE | SDL_RENDERER_PRESENTVSYNC);
+		SDL_SetRenderDrawColor(renderer, 0xaa, 0xaa, 0x3b, 0x00);
+		ret = true;
+	}
+
+	return ret;
 }
