5ce17df4a52f715dafd8a8b474b00fd8a203d873
Aritz <Ari_urd_lar@outlook.com>
2022-01-12 12:43:31 +0100

    InitClose.h eta InitClose.h .c sortu dira, bertan hasieratu eta amaitzeko funtzioak gehituko dira.

---
+++ MUwar/InitClose.h
@@ -0,0 +1,11 @@
+#ifndef INITCLOSE_H
+#define INITCLOSE_H
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdbool.h>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+bool init(SDL_Window* window, SDL_Renderer* renderer, char* titulo, int xpos, int ypos, int width, int height, bool fullscreen);
+#endif // INITCLOSE_H
--- MUwar/src/main.c
+++ MUwar/src/main.c
@@ -7,9 +7,9 @@
 
 #include "tropa.h"
 #include "baldosa.h"
+#include"InitClose.h"
 
-bool init(SDL_Window* window, SDL_Renderer* renderer, char* titulo, int xpos, int ypos, int width, int height, bool fullscreen);
-void render(SDL_Window* window, SDL_Renderer* renderer);
+
 int main(void)
 {
 	SDL_Window* window = NULL;
@@ -29,57 +29,5 @@ int main(void)
 	return 0;
 }
 
-bool init(SDL_Window* window, SDL_Renderer* renderer, char* titulo, int xpos, int ypos, int width, int height, bool fullscreen)
-{
-	int flags = 0;
-	bool ret = true;
 
-	if (fullscreen)flags = SDL_WINDOW_FULLSCREEN_DESKTOP;
-
-	/*-----------------------------------------------------------------------------------------------
-	SDL libreria guztiak hasieratzen ditu, hala ezean errore bat emango du
-	-----------------------------------------------------------------------------------------------*/
-	
-	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
-	{
-		fprintf(stderr, SDL_GetError());
-		ret = false;
-	}
-
-
-	/*-----------------------------------------------------------------------------------------------
-	Lehio bat sortzen du, hala ezean errore bat emango du
-	-----------------------------------------------------------------------------------------------*/
-	if ((window = SDL_CreateWindow(titulo, xpos, ypos, width, height, flags | SDL_WINDOW_RESIZABLE | SDL_WINDOW_FOREIGN)) == NULL)
-	{
-		fprintf(stderr, SDL_GetError());
-		ret = false;
-	}
-
-	/*-----------------------------------------------------------------------------------------------
-	Renderizadore bat sortzen du, hala ezean errore bat emango du
-	-----------------------------------------------------------------------------------------------*/
-	if ((renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE | SDL_RENDERER_PRESENTVSYNC)) == NULL)
-	{
-		fprintf(stderr, SDL_GetError());
-		ret = false;
-	}
-	/*-----------------------------------------------------------------------------------------------
-	Ze kolorearekin margtzea nahi det esaten da, hala ezean errore bat emango du
-	-----------------------------------------------------------------------------------------------*/
-	if (SDL_SetRenderDrawColor(renderer, 0xaa, 0xaa, 0x3b, 0x00) < 0)
-	{
-		fprintf(stderr, SDL_GetError());
-		ret = false;
-	}
-	/*-----------------------------------------------------------------------------------------------
-	Lehioa por defekto txuriz margotzen da, ze kolorearekin margotzea esan ondoren lehioan dagoena guztiz ezabatu eta berriro marrazteko esaten da 
-	-----------------------------------------------------------------------------------------------*/
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-	/*-----------------------------------------------------------------------------------------------
-	jokoa hasteko prest dagoen edo ez itsuliko du
-	-----------------------------------------------------------------------------------------------*/
-	return ret;
-}
 
