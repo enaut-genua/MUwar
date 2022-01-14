ca573777489140db551056c963fbf8645d11daaa
Aritz <Ari_urd_lar@outlook.com>
2022-01-14 23:52:53 +0100

    Tropak dinamikoki mugutzen dira,  funtzioak txukundu gabe daude.

--- MUwar/src/jokoa.c
+++ MUwar/src/jokoa.c
@@ -7,6 +7,10 @@ int TAMAÑOIMAGEN = 100;
 int rango = 2;
 int DetektatutakoTropa;
 int tmpposx, tmpposy;
+int tmpx=0, tmpy=0;
+int tmpinfox, tmpinfoy;
+bool mugitu = false;
+bool XiritsiDa;
 void init(char* titulo, int xpos, int ypos, int width, int height, bool fullscreen) {
 	int flags = 0;
 	if (fullscreen)flags = SDL_WINDOW_FULLSCREEN_DESKTOP;
@@ -17,7 +21,79 @@ void init(char* titulo, int xpos, int ypos, int width, int height, bool fullscre
 		isRunning = true;
 	}
 }
+SDL_Rect laukiakkk;
+int nora_joan(int Hasiera,int Bukaera) {
+	int norantza=0;
+	if (Bukaera - Hasiera > 0)
+	{
+		norantza = 2;
+	}
+	else if (Bukaera - Hasiera < 0)
+	{
+		norantza = -2;
+	}
 
+	return norantza;
+}
+
+void desplazamenduDinamikoa() {
+	if (mugitu==true)
+	{
+		iso(tmpx, tmpy); 
+		laukia(isometric.x, isometric.y, TAMAÑOIMAGEN, TAMAÑOIMAGEN, &laukiakkk);
+		SDL_RenderCopy(renderer, soldado.irudiak[1], NULL, &laukiakkk);
+		printf("\n %d %d %d %d\n", nora_joan(persona.x, tmpinfox), nora_joan(persona.y, tmpinfoy), tmpx, tmpy);
+		if (XiritsiDa == false)tmpx += nora_joan(persona.x, tmpinfox);
+		if ( XiritsiDa == true)
+		{
+			XiritsiDa = false;
+			tmpy += nora_joan(persona.y, tmpinfoy);
+		}
+
+		// dago garrita == baño ez litzateke horrela izan behar, <= edo >= izan beharko zen
+		//gauza da luzeegia geratzen dela GOGORATU HAU ZUZENTZEKO DAGOETA, NORA_JOAN FUNTZIOKO ABIADURA AZKARTZEAN SE VA A LA MIERDA
+		if (tmpx == ((TAMAÑOIMAGEN * 0.5) * tmpinfox) + mapPos.x)
+		{
+			XiritsiDa = true;
+			if (tmpy == (TAMAÑOIMAGEN * 0.5) * tmpinfoy - mapPos.y)
+			{
+				PERTSONAK[infoPos.y][infoPos.x] = DetektatutakoTropa;
+				mugitu = false;
+				tmpx = 0; tmpy = 0;
+			}
+		}
+		
+	}
+}
+void iritsiDa(int orgX,int destX, int orgY,int destY) {
+	if (orgX <= destX)
+	{
+		XiritsiDa = true;
+		if (orgY <= destY)
+		{
+			PERTSONAK[infoPos.y][infoPos.x] = DetektatutakoTropa;
+			mugitu = false;
+			tmpx = 0; tmpy = 0;
+		}
+	}
+}
+void update() {
+
+}
+void Desplazatu() {
+
+	PERTSONAK[tmpposy][tmpposx] = 0;
+	persona.x = tmpposx;
+	persona.y = tmpposy;
+	tmpx = (TAMAÑOIMAGEN * 0.5) * persona.x + mapPos.x;
+	tmpy = (TAMAÑOIMAGEN * 0.5) * persona.y - mapPos.y;
+	mugitu = true;
+	tmpinfox = infoPos.x;
+	tmpinfoy= infoPos.y;
+	
+	
+	//printf("\n %d %d %d %d\n", persona.x, persona.y, tmpinfox, tmpinfoy);
+}
 void handleEvents() {
 	MousePos();
 
@@ -44,10 +120,8 @@ void handleEvents() {
 			}
 			else if (editMap) {
 				if (RANGO_JOKALARIARENA[infoPos.y][infoPos.x] == 1) {
-					PERTSONAK[tmpposy][tmpposx] = 0;
-					persona.x = infoPos.x;
-					persona.y = infoPos.y;
-					PERTSONAK[persona.y][persona.x] = DetektatutakoTropa;
+
+					Desplazatu();
 					printf("\nTROPA MUGITU DA DA\n");
 				}
 				RangoaEzabatu();
@@ -64,18 +138,14 @@ void handleEvents() {
 				isRunning = false; 
 				break;
 			case SDLK_p:
-				persona.y = TALE_Y-1;
+				printf("\nMUGITU DAITEKE TROPA BAT\n");
 				editPeople = true;  break;
 			case SDLK_o:
-				editPeople = false;  break;
+				editPeople = false; printf("\nEZIN DA MUGITU TROPA\n");  break;
 			case SDLK_w:mapPos.x += (int)(TAMAÑOIMAGEN * 0.5); mapPos.y -= (int)(TAMAÑOIMAGEN * 0.5); break;
 			case SDLK_s:mapPos.x -= (int)(TAMAÑOIMAGEN * 0.5); mapPos.y += (int)(TAMAÑOIMAGEN * 0.5); break;
 			case SDLK_d:mapPos.x -= (int)(TAMAÑOIMAGEN * 0.5); mapPos.y -= (int)(TAMAÑOIMAGEN * 0.5); break;
 			case SDLK_a:mapPos.x += (int)(TAMAÑOIMAGEN * 0.5); mapPos.y += (int)(TAMAÑOIMAGEN * 0.5); break;
-			case SDLK_RIGHT:Desplazatu(tmpposx, tmpposy,1, 0, 2); break;
-			case SDLK_LEFT:Desplazatu(-1, 0, 3); break;
-			case SDLK_DOWN:Desplazatu(0, 1, 1); break;
-			case SDLK_UP:Desplazatu(0, -1, 0); break;
 			case SDLK_e:
 
 				break;
@@ -105,11 +175,6 @@ void RangoaIpini(int rango) {
 					RANGO_JOKALARIARENA[infoPos.y + y - rango][infoPos.x + x - rango] = 1;
 			}
 		}
-	}
-}
-void Desplazatu(int x, int y, int orientazioa) {
-	if (editPeople == true) {
-
 	}
 }
 void MousePos(void) {
@@ -188,6 +253,7 @@ void render() {
 	SDL_RenderClear(renderer);
 	erakutsiTale(mousePos.x, mousePos.y);
 	Mapa();
+	desplazamenduDinamikoa();
 	SDL_RenderPresent(renderer);
 
 }
@@ -208,6 +274,4 @@ void clean() {
 bool running() {
 	return isRunning;
 }
-void update() {
-	
-}
+
