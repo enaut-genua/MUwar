0650e8089b0befd1746135db359b72e8b82c99c5
JONMA06 <97883168+JONMA06@users.noreply.github.com>
2022-01-19 10:12:05 +0100

    Update mapa_modeloak.h

--- MUwar/src/mapa_modeloak.h
+++ MUwar/src/mapa_modeloak.h
@@ -5,7 +5,50 @@
 
 
 TerrenoMotak mapa_1 = {
-	Larrea
+Base_urdina, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Ibaia, Larrea, Mendia, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea,
+Larrea, Larrea, Larrea, Larrea, Larrea, Herria, Larrea, Larrea, Ibaia, Mendia, Mendia, Larrea, Larrea, Larrea, Basoa, Larrea, Larrea, Basoa, Larrea, Larrea,
+Larrea, Larrea, Basamortua, Larrea, Larrea, Larrea, Larrea, Larrea, Ibaia, Larrea, Mendia, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea,
+Larrea, Basamortua, Basamortua, Basamortua, Larrea, Larrea, Larrea, Larrea, Ibaia, Larrea, Mendia, Mendia, Larrea, Basoa, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea,
+Larrea, Larrea, Basamortua, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Mendia, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea,
+Larrea, Larrea, Larrea, Larrea, Larrea Larrea, Larrea, Basamortua, Larrea, Larrea, Basamortua, Basamortua, Basamortua, Larrea, Herria, Larrea, Larrea, Basoa, Larrea, Larrea,
+Larrea, Larrea, Larrea, Larrea, Larrea, Herria, Basamortua, Basamortua, Basamortua, Basamortua, Basamortua, Basamortua, Larrea, Larrea, Larrea, Larrea, Larrea, Mendia, Mendia, Larrea,
+Ibaia, Ibaia, Larrea, Larrea, Larrea, Larrea, Basamortua, Basamortua, Larrea, Larrea, Larrea, Basamortua, Larrea, Larrea, Larrea, Larrea, Larrea, Mendia, Mendia, Mendia,
+Larrea, Ibaia, Ibaia, Larrea, Larrea, Larrea, Larrea, Basamortua, Larrea, Basoa, Larrea, Basamortua, Basamortua, Larrea, Larrea, Larrea, Mendia, Mendia, Larrea, Larrea,
+Larrea, Larrea, Ibaia, Larrea, Larrea, Larrea, Larrea, Basamortua, Larrea, Larrea, Larrea, Basamortua, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea,
+Larrea, Larrea, Ibaia, Larrea, Larrea, Larrea, Basamortua, Basamortua, Basamortua, Basamortua, Basamortua, Basamortua, Herria, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea,
+Larrea, Larrea, Ibaia, Ibaia, Larrea, Larrea, Larrea, Larrea, Larrea, Basamortua, Basamortua, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Basamortua, Larrea, Larrea,
+Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Basamortua, Basamortua, Basamortua, Larrea,
+Larrea, Larrea, Larrea, Larrea, Herria, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Basamortua, Larrea, Larrea,
+Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Mendia, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea,
+Larrea, Basoa, Larrea, Larrea, Larrea, Larrea, Basoa, Larrea, Mendia, Mendia, Larrea, Larrea, Larrea, Larrea, Ibaia, Larrea, Herria, Larrea, Larrea, Larrea,
+Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Mendia, Larrea, Larrea, Larrea, Larrea, Larrea, Ibaia, Larrea, Larrea, Larrea, Larrea, Larrea,
+Larrea, Larrea, Basoa, Larrea, Larrea, Basoa, Larrea, Mendia, Mendia, Larrea, Larrea, Larrea, Larrea, Larrea, Ibaia, Larrea, Larrea, Larrea, Larrea, Larrea,
+Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Mendia, Larrea, Larrea, Larrea, Larrea, Larrea, Ibaia, Larrea, Larrea, Larrea, Larrea, Larrea,
+Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Mendia, Larrea, Larrea, Larrea, Larrea, Larrea, Ibaia, Larrea, Larrea, Larrea, Larrea, Base_gorria,
+};
+
+TerrenoMotak mapa_2 =
+{
+Base_urdina, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea,
+Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Basamortua, Basamortua, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Herria, Larrea,
+Larrea, Larrea, Larrea, Basoa, Larrea, Larrea, Basamortua, Basamortua, Basamortua, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Basoa, Larrea, Larrea,
+Larrea, Larrea, Larrea, Larrea, Larrea, Basamortua, Basamortua, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Basamortua, Larrea, Larrea, Larrea, Larrea,
+Larrea, Larrea, Larrea, Larrea, Basamortua, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Basamortua, Basamortua, Basamortua, Larrea, Larrea, Larrea,
+Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Ibaia, Ibaia, Ibaia, Ibaia, Ibaia, Larrea, Larrea, Larrea, Basamortua, Basamortua, Larrea, Larrea, Larrea,
+Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Ibaia, Ibaia, Mendia, Mendia, Mendia, Mendia, Larrea, Larrea, Larrea, Larrea, Basamortua, Basamortua, Larrea, Larrea,
+Larrea, Larrea, Larrea, Larrea, Larrea, Ibaia, Ibaia, Larrea, Larrea, Mendia, Mendia, Mendia, Mendia, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea,
+Larrea, Herria, Basoa, Larrea, Larrea, Ibaia, Mendia, Larrea, Larrea, Basoa, Larrea, Basoa, Mendia, Mendia, Ibaia, Larrea, Larrea, Larrea, Larrea, Larrea,
+Larrea, Larrea, Larrea, Larrea, Larrea, Ibaia, Mendia, Mendia, Basoa, Herria, Herria, Larrea, Mendia, Mendia, Ibaia, Larrea, Larrea, Larrea, Larrea, Larrea,
+Larrea, Larrea, Larrea, Larrea, Larrea, Ibaia, Mendia, Mendia, Larrea, Herria, Herria, Basoa, Mendia, Mendia, Ibaia, Larrea, Larrea, Larrea, Larrea, Larrea,
+Larrea, Larrea, Basamortua, Larrea, Larrea, Ibaia, Mendia, Mendia, Larrea, Basoa, Larrea, Larrea, Larrea, Mendia, Ibaia, Larrea, Larrea, Larrea, Larrea, Larrea,
+Larrea, Basamortua, Basamortua, Larrea, Larrea, Larrea, Larrea, Mendia, Mendia, Mendia, Mendia, Larrea, Larrea, Ibaia, Ibaia, Larrea, Larrea, Larrea, Basoa, Larrea,
+Larrea, Basamortua, Basamortua, Larrea, Larrea, Larrea, Larrea, Larrea, Mendia, Mendia, Mendia, Mendia, Ibaia, Ibaia, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea,
+Larrea, Larrea, Basamortua, Basamortua, Larrea, Larrea, Larrea, Larrea, Ibaia, Ibaia, Ibaia, Ibaia, Ibaia, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea,
+Larrea, Larrea, Larrea, Basamortua, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Basamortua, Basamortua, Basamortua, Larrea, Larrea,
+Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Basamortua, Basamortua, Basamortua, Larrea, Larrea, Larrea,
+Larrea, Larrea, Basoa, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Basamortua, Larrea, Basamortua, Larrea, Larrea, Larrea, Larrea,
+Larrea, Larrea, Larrea, Larrea, Ibaia, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Herria, Larrea, Larrea, Larrea, Basoa, Larrea, Larrea, Larrea, Larrea,
+Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Larrea, Base_gorria,
 };
 
 
