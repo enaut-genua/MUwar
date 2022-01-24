3b82e3cf1ba9328750511d9655cb0fc8121cdbaa
Aritz <Ari_urd_lar@outlook.com>
2022-01-24 08:24:59 +0100

    ttf funtzioak sartu dira

--- MUwar/src/renderizadorea.c
+++ MUwar/src/renderizadorea.c
@@ -97,6 +97,46 @@ const char* LEHIOAREN_IZENA = "MUwar";
  *	END: Konstanteak
  */
 
+
+ //______________________________________________TTF_____________________________________________________________________//
+TTF_Font* font = 0;
+void textuaIdatzi(int x, int y, char* str)
+{
+	SDL_Surface* textSurface;
+	SDL_Texture* mTexture;
+	SDL_Color textColor = { 0, 0, 0 };
+	SDL_Rect src, dst;
+	SDL_Renderer* gRenderer;
+
+	if (font == 0) return;
+	gRenderer = getRenderer();
+	textSurface = TTF_RenderText_Solid(font, str, textColor);
+	mTexture = SDL_CreateTextureFromSurface(gRenderer, textSurface);
+	src.x = 0; dst.x = x;
+	src.y = 0; dst.y = y;
+	src.w = dst.w = textSurface->w;
+	src.h = dst.h = textSurface->h;
+	SDL_RenderCopy(gRenderer, mTexture, &src, &dst);
+	SDL_FreeSurface(textSurface);
+	SDL_DestroyTexture(mTexture);
+}
+void textuaGaitu(void) {
+	font = TTF_OpenFontIndex("arial.ttf", 20, 0);
+	if (!font)
+	{
+		printf("TTF_OpenFontIndex: %s\n", TTF_GetError());
+		// handle error
+	}
+}
+void textuaDesgaitu(void)
+{
+	if (font != 0) TTF_CloseFont(font);
+	font = 0;
+}
+SDL_Renderer* getRenderer(void) { return renderer; }
+//______________________________________________TTF_____________________________________________________________________//
+
+
 bool render_sortu(int xpos, int ypos, int width, int height, bool fullscreen)
 {
 	bool dena_ondo = true;
